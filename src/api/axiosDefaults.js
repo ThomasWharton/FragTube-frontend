9f4c1f8c26c5d0aa4093fbe3dee00aadb7e00550
ThomasWharton <thomaswharton7@hotmail.com>
2024-01-29 19:38:46 +0000

    Feat: Set up axios interceptors

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://frag-tube-0da3a5b7d9cc.herokuapp.com/";
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
