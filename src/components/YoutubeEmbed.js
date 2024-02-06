751bb7ff3d018041ffc1219374cc7161c476af36
ThomasWharton <thomaswharton7@hotmail.com>
2024-02-06 01:59:45 +0000

    Feat: Add like and comment icons/count

--- src/components/YoutubeEmbed.js
+++ src/components/YoutubeEmbed.js
@@ -2,6 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "../styles/YoutubeEmbed.module.css"
 
+// This component was taken from https://dev.to/bravemaster619/simplest-way-to-embed-a-youtube-video-in-your-react-app-3bk2
+
 const YoutubeEmbed = ({ embedId, title }) => (
   
   <div className={styles.VideoResponsive}>
--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,6 +1,13 @@
 import React from "react";
 import styles from "../../styles/Post.module.css";
-import { Card, Media } from "react-bootstrap";
+import {
+  Row,
+  Card,
+  Media,
+  OverlayTrigger,
+  Tooltip,
+  Col,
+} from "react-bootstrap";
 import Avatar from "../../components/Avatar";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { Link } from "react-router-dom";
@@ -16,7 +23,6 @@ const Post = (props) => {
     likes_count,
     like_id,
     title,
-    category,
     embed_id,
     content,
     updated_at,
@@ -26,27 +32,63 @@ const Post = (props) => {
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
 
-  return <Card className={styles.Post}>
-    <Card.Body>
+  return (
+    <Card className={styles.Post}>
+      <Card.Body>
         <Media className="align-items-center justify-content-between">
-            <Link to={`/profiles/${profile_id}`} className={styles.Link}>
-                <Avatar src={profile_image} height={55} />
-                {owner}
-            </Link>
-            <div className="d-flex align-items-center">
-                <span>{updated_at}</span>
-                {is_owner && postPage && "..."}
-            </div>
+          <Link to={`/profiles/${profile_id}`} className={styles.Link}>
+            <Avatar src={profile_image} height={55} />
+            {owner}
+          </Link>
+          <div className="d-flex align-items-center">
+            <span>{updated_at}</span>
+            {is_owner && postPage && "..."}
+          </div>
         </Media>
-    </Card.Body>
-    <Card.Body className={styles.Video}>
-      <YoutubeEmbed embedId={embed_id} />
-    </Card.Body>
-    <Card.Body>
+      </Card.Body>
+      <Card.Body className={styles.Video}>
+        <YoutubeEmbed embedId={embed_id} />
+      </Card.Body>
+      <Card.Body>
         {title && <Card.Title className="text-center">{title}</Card.Title>}
         {content && <Card.Text>{content}</Card.Text>}
-    </Card.Body>
-  </Card>
+        <Row>
+          <Col>
+            {is_owner ? (
+              <OverlayTrigger
+                placement="top"
+                overlay={<Tooltip>You can't like your own post!</Tooltip>}
+              >
+                <i className="far fa-heart" />
+              </OverlayTrigger>
+            ) : like_id ? (
+              <span onClick={() => {}}>
+                <i className={`fas fa-heart ${styles.Heart}`} />
+              </span>
+            ) : currentUser ? (
+              <span onClick={() => {}}>
+                <i className={`far fa-heart ${styles.HeartOutline}`} />
+              </span>
+            ) : (
+              <OverlayTrigger
+                placement="top"
+                overlay={<Tooltip>Log in to like posts!</Tooltip>}
+              >
+                <i className="far fa-heart" />
+              </OverlayTrigger>
+            )}
+            {likes_count}
+          </Col>
+          <Col>
+            <Link to={`/posts/${id}`}>
+              <i className={`far fa-comments ${styles.Comment}`} />
+            </Link>
+            {comments_count}
+          </Col>
+        </Row>
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default Post;
