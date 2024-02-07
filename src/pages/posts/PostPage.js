f062b2c0e1a36cdbdb9a4a1c19032adf2c792c95
ThomasWharton <thomaswharton7@hotmail.com>
2024-02-07 01:36:27 +0000

    Feat: Add functionality to edit/delete comments

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -9,6 +9,8 @@ import Asset from "../../components/Asset";
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import Comment from "../comments/Comment";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
 
 function PostPage() {
   const { id } = useParams();
@@ -21,9 +23,9 @@ function PostPage() {
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: post }, {data: comments}] = await Promise.all([
+        const [{ data: post }, { data: comments }] = await Promise.all([
           axiosReq.get(`/posts/${id}`),
-          axiosReq.get(`/comments/?post=${id}`)
+          axiosReq.get(`/comments/?post=${id}`),
         ]);
         setPost({ results: [post] });
         setComments(comments);
@@ -58,9 +60,20 @@ function PostPage() {
               "Comments"
             ) : null}
             {comments.results.length ? (
-              comments.results.map(comment => (
-                <Comment key={comment.id} {...comment} />
-              ))
+              <InfiniteScroll
+                children={comments.results.map((comment) => (
+                  <Comment
+                    key={comment.id}
+                    {...comment}
+                    setPost={setPost}
+                    setComments={setComments}
+                  />
+                ))}
+                dataLength={comments.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!comments.next}
+                next={() => fetchMoreData(comments, setComments)}
+              />
             ) : currentUser ? (
               <span>No comments yet, be the first to comment!</span>
             ) : (
