7a13e2553b4bac8ebb0d6c9e932b843ce24156fb
ThomasWharton <thomaswharton7@hotmail.com>
2024-02-07 21:53:47 +0000

    Feat: Make username a link to profile page

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import styles from "../../styles/Profile.module.css"
-import { useCurrentUser } from '../../contexts/CurrentUserContext';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
-import Avatar from '../../components/Avatar';
-import { Button } from 'react-bootstrap';
-import { useSetProfileData } from '../../contexts/ProfileDataContext';
+import React from "react";
+import styles from "../../styles/Profile.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import Avatar from "../../components/Avatar";
+import { Button } from "react-bootstrap";
+import { useSetProfileData } from "../../contexts/ProfileDataContext";
 
 const Profile = (props) => {
-  const { profile, mobile, imageSize=55 } = props;
+  const { profile, mobile, imageSize = 55 } = props;
   const { id, following_id, image, owner } = profile;
 
   const currentUser = useCurrentUser();
@@ -19,35 +19,39 @@ const Profile = (props) => {
     <div
       className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}
     >
-        <div>
-            <Link className="align-self-center" to={`/profiles/${id}`}>
-                <Avatar src={image} height={imageSize} />
-            </Link>
-        </div>
-        <div className={`mx-2 ${styles.WordBreak}`}>
-            <strong>{owner}</strong>
-        </div>
-        <div className={`text-right ${!mobile && 'ml-auto'}`}>
-            {!mobile && currentUser && !is_owner && (
-                following_id ? (
-                    <Button
-                      className={styles.UnfollowButton}
-                      onClick={() => handleUnfollow(profile)}
-                    >
-                      unfollow
-                    </Button>
-                ) : (
-                    <Button
-                      className={styles.FollowButton}
-                      onClick={() => handleFollow(profile)}
-                    >
-                      follow
-                    </Button>
-                )
-            )}
-        </div>
+      <div>
+        <Link className="align-self-center" to={`/profiles/${id}`}>
+          <Avatar src={image} height={imageSize} />
+        </Link>
+      </div>
+      <Link
+        className={`mx-2 ${styles.WordBreak} ${styles.Link}`}
+        to={`/profiles/${id}`}
+      >
+        <strong>{owner}</strong>
+      </Link>
+      <div className={`text-right ${!mobile && "ml-auto"}`}>
+        {!mobile &&
+          currentUser &&
+          !is_owner &&
+          (following_id ? (
+            <Button
+              className={styles.UnfollowButton}
+              onClick={() => handleUnfollow(profile)}
+            >
+              unfollow
+            </Button>
+          ) : (
+            <Button
+              className={styles.FollowButton}
+              onClick={() => handleFollow(profile)}
+            >
+              follow
+            </Button>
+          ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
