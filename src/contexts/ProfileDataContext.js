3d3684ca877d25de7a52ec25612151fcbe40366e
ThomasWharton <thomaswharton7@hotmail.com>
2024-02-07 21:04:57 +0000

    Fix: Issue of unfollow request returning 404

--- src/contexts/ProfileDataContext.js
+++ src/contexts/ProfileDataContext.js
@@ -45,7 +45,7 @@ export const ProfileDataProvider = ({ children }) => {
 
       const handleUnfollow = async (clickedProfile) => {
         try{
-          await axiosRes.delete(`/followers/${clickedProfile.following_id}/`);
+          await axiosRes.delete(`/followers/${clickedProfile.following_id}`);
 
           setProfileData(prevState => ({
             ...prevState,
