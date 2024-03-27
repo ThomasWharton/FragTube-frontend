27190ccd9c08d75f2dfb2c79ad7e10458fa448a1
ThomasWharton <thomaswharton7@hotmail.com>
2024-03-27 01:21:27 +0000

    Feat: Add functionality/styling to rating

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
+import styles from "../styles/Rating.module.css";
 
 const Rating = () => {
-  return (
-    <div>Rating</div>
-  )
-}
+  const [rating, setRating] = useState(null);
+  const [hover, setHover] = useState(null);
 
-export default Rating
+  return (
+    <div className={styles.Rating}>
+      {[...Array(5)].map((star, i) => {
+        const ratingValue = i + 1;
+
+        return (
+          <label>
+            <input
+              type="radio"
+              name="rating"
+              value={ratingValue}
+              onClick={() => setRating(ratingValue)}
+            />
+            <i
+              className={`fa fa-star ${styles.Star}`}
+              style={{
+                color: ratingValue <= (hover || rating) ? "#FF0000" : "#E4E4E4"
+              }}
+              onMouseEnter={() => setHover(ratingValue)}
+              onMouseLeave={() => setHover(null)}
+            />
+          </label>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Rating;
